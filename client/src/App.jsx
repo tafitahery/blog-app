02193f69b1b0a6c61578dcbd0f868d32a2760f00
Tafita Hery <tafitahery@gmail.com>
2023-10-11 15:10:24 +0300

    React Router Dom 6 Tutorial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,59 @@
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Write from './pages/Write';
+import Single from './pages/Single';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/post/:id',
+        element: <Single />,
+      },
+      {
+        path: '/write',
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
